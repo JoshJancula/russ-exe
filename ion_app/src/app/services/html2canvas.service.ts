9d580ca50a0da47b43665a2a435faac59a7ef25f
Joshua Jancula <josh.jancula@sebrio.com>
2020-01-22 16:30:48 -0500

    canvas service

---
+++ ion_app/src/app/services/html2canvas.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import html2canvas from 'html2canvas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Html2CanvasService {
+  constructor() {
+  }
+
+  public async getCanvas(ele: any) {
+    if (!ele) {
+      return;
+    }
+    const option = { height: 3200, allowTaint: true, useCORS: true };
+    return html2canvas(ele, option).then((canvas: any) => {
+      if (canvas) {
+        return canvas;
+      }
+      return null;
+    }).catch((res: any) => {
+      console.log(res);
+      return res;
+    });
+  }
+}
--- ion_app/src/app/services/pdf.service.ts
+++ ion_app/src/app/services/pdf.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import * as jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-import { environment } from 'src/environments/environment.prod';
+// import html2canvas from 'html2canvas';
+import { environment } from 'src/environments/environment';
 import { ElectronService } from 'ngx-electron';
+import { Html2CanvasService } from './html2canvas.service';
+// declare let html2canvas: any;
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +15,19 @@ export class PdfService {
   private printIframe: HTMLIFrameElement | any;
   private debug: boolean = false;
 
-  constructor(private electronService: ElectronService) { }
+  constructor(private electronService: ElectronService, private canvasService: Html2CanvasService) { }
 
   public generatePDF(action: string, div: HTMLElement, title: string, totalRows?: number): Promise<void> {
     return new Promise((resolve, reject) => {
-      html2canvas(div, { height: 3200 }).then((canvas: any) => {
+
+      this.html2canvas(div).then((canvas: any) => {
         const imgData = canvas.toDataURL('image/png');
         const pdf = new jsPDF('p', 'mm', 'a4', true);
         const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = pdf.internal.pageSize.getHeight() + 40;
+        const pdfHeight = pdf.internal.pageSize.getHeight() + 140;
         const position = window.innerWidth < 600 ? -80 : window.innerWidth >= 600 && window.innerWidth < 800 ? -60 : -50;
         setTimeout(() => {
-          pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, pdfHeight, '', 'FAST');
+          pdf.addImage(imgData, 'PNG', 0, null, pdfWidth, pdfHeight, '', 'FAST');
           if (action === 'download') {
             if (!environment.isElectron || !this.debug) {
               try {
@@ -33,17 +37,7 @@ export class PdfService {
                 alert('error saving pdf.... ' + JSON.stringify(e));
                 resolve();
               }
-              // // pdf.output('save', title);
-              // const reader = new FileReader();
-              // reader.readAsDataURL(pdf.output('blob'));
-              // reader.onloadend = () => {
-              //   const base64data = reader.result;
-              //   console.log(base64data);
-              // // };
-              // const b = pdf.output('blob');
-              // console.log('blobl  ', b);
             } else {
-              // console.log('blob.... ', pdf.output('datauristring').toString());
               this.electronService.ipcRenderer.send('save-pdf', imgData);
               this.electronService.ipcRenderer.on('pdf-complete', (event, args) => {
                 if (args.err) {
@@ -64,6 +58,24 @@ export class PdfService {
         }, 300);
       }).catch((err: any) => {
         reject(err);
+      });
+    });
+  }
+
+  public html2canvas(div: HTMLElement): Promise<any> {
+    // this is ridiculous but there is a bug with ionic and
+    // html2pdf over shadowed rood vars in duplicated stylesheets
+    // so we need to do this and have that service to accomodate
+    return new Promise((resolve, reject) => {
+      const element = document.getElementById('html2canvas');
+      const targetElement = div.cloneNode(true);
+      element.appendChild(targetElement);
+      this.canvasService.getCanvas(element.firstChild).then((c: any) => {
+        resolve(c);
+        element.firstChild.remove();
+      }).catch((res: any) => {
+        console.log(res);
+        reject(res);
       });
     });
   }
